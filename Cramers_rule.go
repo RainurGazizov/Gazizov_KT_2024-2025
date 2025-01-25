040ccf7ca10eccb0f846b21edcc2af26dbf8cc04
RainurGazizov <rainurrrr2000@mail.ru>
2025-01-25 15:36:09 +0300

    Cramers_rule.go

--- Cramers_rule.go
+++ Cramers_rule.go
@@ -7,132 +7,25 @@ import (
 	"time"
 )
 
-type Matrix [][]float64
-
-func (m Matrix) Rows() int {
-	return len(m)
-}
-
-func (m Matrix) Columns() int {
-	return len(m[0])
-}
-
-func (m Matrix) IsSquare() bool {
-	return m.Columns() == m.Rows()
-}
-
-func (m Matrix) IsMatrix() bool {
-	if m.Rows() == 0 {
-		return false
-	}
-
-	for _, row := range m {
-		if len(m[0]) != len(row) {
-			return false
-		}
-	}
-	return true
-}
-
-func InBetween(i, min, max int) bool {
-	if (i >= min) && (i <= max) {
-		return true
-	} else {
-		return false
-	}
-}
-
-func (m Matrix) ExcludeColumn(col_index int) (Matrix, error) {
-
-	if !InBetween(col_index, 1, m.Columns()) {
-		return Matrix{}, fmt.Errorf("input not in range")
-	}
-
-	result := make(Matrix, m.Rows())
-	for i, row := range m {
-		for j, el := range row {
-			if j == col_index-1 {
-				continue
-			}
-			result[i] = append(result[i], el)
-		}
-	}
-	return result, nil
-}
-
-func (m Matrix) ExcludeRow(row_index int) (Matrix, error) {
-	if !InBetween(row_index, 1, m.Rows()) {
-		return Matrix{}, fmt.Errorf("input not in range")
-	}
-
-	var result Matrix
-	for i, r := range m {
-		if i == row_index-1 {
-			continue
-		}
-		result = append(result, r)
-	}
-	return result, nil
-}
-
-func (m Matrix) Det() (float64, error) {
-
-	if !m.IsMatrix() || !m.IsSquare() {
-		return -1, fmt.Errorf("determinant is not defined for the input [Matrix: %t][Square: %t]",
-			m.IsMatrix(), m.IsSquare())
-	}
-
-	if m.Rows() == 2 {
-		return m[0][0]*m[1][1] - m[0][1]*m[1][0], nil
-	}
-
-	// исключаем первый столбец
-	partial_matrix, err := m.ExcludeRow(1)
-	if err != nil {
-		return -1, err
-	}
-
-	var temp float64 = 0
-
-	// раскладываем по элементам первого столбца
-	for i, el := range m[0] {
-
-		reduced_matrix, err := partial_matrix.ExcludeColumn(i + 1)
-		if err != nil {
-			return -1, err
-		}
-
-		partial_det, err := reduced_matrix.Det()
-		if err != nil {
-			return -1, err
-		}
-
-		temp = temp + partial_det*el*math.Pow(-1, float64(i))
-	}
-
-	return temp, nil
-}
-
 // Размерность матрицы
-const N = 10
+const N = 100
 
 func main() {
 	start := time.Now()
 
 	//создание матрицы
-	data := make([][]float64, N)
+	data := [N][N]float64{}
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	for i := 0; i < N; i++ {
-		data[i] = make([]float64, N)
 		for j := 0; j < N; j++ {
 			//матрица, состоящая из случайных элементов от -10 до 10
 			data[i][j] = -10 + r.Float64()*20
 		}
 	}
 
-	b := make([]float64, N)
+	b := [N]float64{}
 
 	for i := 0; i < N; i++ {
 		//столбец, состоящий из случайных элементов от -10 до 10
@@ -143,11 +36,11 @@ func main() {
 
 	//fmt.Println(b)
 
-	det, _ := Matrix(data).Det()
+	D := det(data)
 
 	//fmt.Println(det)
 
-	if det == 0 {
+	if D == 0 {
 		fmt.Println("Матрица вырождена")
 		return
 	}
@@ -155,14 +48,7 @@ func main() {
 	x := make([]float64, N)
 
 	for i := 0; i < N; i++ {
-		temp := make([][]float64, N)
-
-		for s := 0; s < N; s++ {
-			temp[s] = make([]float64, N)
-			for j := 0; j < N; j++ {
-				temp[s][j] = data[s][j]
-			}
-		}
+		temp := data
 
 		for k := 0; k < N; k++ {
 			temp[k][i] = b[k]
@@ -170,8 +56,8 @@ func main() {
 
 		//printMatrix(temp)
 
-		det_i, _ := Matrix(temp).Det()
-		x[i] = det_i / det
+		det_i := det(temp)
+		x[i] = det_i / D
 	}
 
 	fmt.Println("Решение СЛАУ")
@@ -181,7 +67,7 @@ func main() {
 	fmt.Printf("Время выполнения кода: %v\n", duration)
 }
 
-func printMatrix(X [][]float64) {
+func printMatrix(X [N][N]float64) {
 	for i := range len(X) {
 		for j := range len(X) {
 			fmt.Printf("%10f", X[i][j])
@@ -189,4 +75,39 @@ func printMatrix(X [][]float64) {
 		fmt.Println()
 	}
 	fmt.Println()
+}
+
+func det(a [N][N]float64) float64 {
+	det := 1.0
+
+	for k := 0; k < N; k++ {
+		iMax := k
+		max := math.Abs(a[k][k])
+		for i := k + 1; i < N; i++ {
+			if abs := math.Abs(a[i][k]); abs > max {
+				iMax = i
+				max = abs
+			}
+		}
+
+		if a[iMax][k] == 0 {
+			return 0
+		}
+
+		if iMax != k {
+			a[k], a[iMax] = a[iMax], a[k]
+			det *= -1
+		}
+
+		det *= a[k][k]
+
+		for i := k + 1; i < N; i++ {
+			for j := k + 1; j < N; j++ {
+				a[i][j] -= a[k][j] * (a[i][k] / a[k][k])
+			}
+			a[i][k] = 0
+		}
+	}
+
+	return det
 }
